af89057cd2e534718eaca72eb445fc364451c398
Ivan Honcharuk <ivan.honcharuk@student.tuke.sk>
2025-05-09 10:27:24 +0200

    root redirect

---
+++ src/main/java/sk/tuke/gamestudio/controller/RootController.java
@@ -0,0 +1,13 @@
+package sk.tuke.gamestudio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String redirectToAuth() {
+        return "redirect:/auth";
+    }
+}
